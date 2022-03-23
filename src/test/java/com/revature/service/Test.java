30e0962ee3ef93891ffce6a5cbe9ec1d7af37c61
Bach Tran <bach.tran@revature.com>
2022-03-23 09:41:41 -0500

    Modify gradle.yml so that the gradle wrapper can be set to executable permissions while running the Github Actions workflow

---
+++ src/test/java/com/revature/service/Test.java
@@ -0,0 +1,12 @@
+package com.revature.service;
+
+import org.junit.jupiter.api.Assertions;
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+
+}
